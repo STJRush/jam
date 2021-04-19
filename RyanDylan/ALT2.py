7034b2c87be808b25a76b43123219bed90dfd3f7
RyanCleary00 <71330065+RyanCleary00@users.noreply.github.com>
2021-04-19 09:46:24 +0100

    Update ALT2.py

--- RyanDylan/ALT2.py
+++ RyanDylan/ALT2.py
@@ -1,44 +1,41 @@
-# install pygal and lxml packages before running this code
+# Install pygal and lxml packages before running this code
 import pygal
 import csv
 
-#Turning pairs of columns into x,y series for graphing
+# Turning pairs of columns into x,y series for graphing
 ######################################################
 
-#series name = [[int(first column of data), int(second column of data)] for column in reader]
-#REMEMBER: column[0] is the first column, column[1] is the second etc.
+# Series name = [[int(first column of data), int(second column of data)] for column in reader]
+# REMEMBER: column[0] is the first column, column[1] is the second etc.
 
-#opens the csv
+# Opens the csv
 f = open("myData.csv", newline = '')
 reader = csv.reader(f)
-#makes a list from the first and second columns
+# Makes a list from the first and second columns
 dataListedSeriesA = [[int(column[0]), int(column[1])] for column in reader]
 f.close()
 
-#opens the csv
+# Repeat for Series B
 f = open("myData.csv", newline = '')
 reader = csv.reader(f)
-#makes a list from the first and third columns
 dataListedSeriesB = [[int(column[0]), int(column[2])] for column in reader]
 f.close()
 
-#you have to close the file between reading each series, no idea why.
+# You have to close the file between reading each series, no idea why.
 
-
-#Graphing the series using pygal
-######################################################
+# Graphing the series using pygal
 
 disChart = pygal.XY(stroke=False)
 
-#chart title
+# Chart title
 disChart.title = 'Correlation'
 
-#disChart.add('Series Name', [lists of data points])
+# disChart.add('Series Name', [lists of data points])
 disChart.add('A', dataListedSeriesA)
 disChart.add('B', dataListedSeriesB)
 
-#save top a vector graphics file
+# Save top a vector graphics file
 disChart.render_to_file('chart.svg')
 
-#show in your bcolumnser
+# Show in your browser
 disChart.render_in_browser()

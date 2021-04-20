537b253cefaae2c2e3d91e65725605ba7144d1bc
ConnorRush900 <16cbyrne@stjosephsrush.com>
2021-04-20 13:51:37 +0100

    Update frigginmodeandstuff.py

--- PIC Folder/frigginmodeandstuff.py
+++ PIC Folder/frigginmodeandstuff.py
@@ -1,1 +1,15 @@
 connor put the thing here
+ok love you x
+
+
+y= [784400000,156880000,5490800000,392200000,392200000,235320000,78440000]
+y.sort()
+L1=[]
+i = 0
+while i < len(y) :
+    L1.append(y.count(y[i]))
+    i += 1
+    d1 = dict(zip(y, L1))
+    d2={k for (k,v) in d1.items() if v == max(L1) }
+ 
+print("Mode(s) is/are :" + str(d2))

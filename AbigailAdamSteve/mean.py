336f6816fdfee1b48be7e27f40676670c3627378
AdamIreland <16AFerguson@stjosephsrush.com>
2021-04-22 09:41:44 +0100

    Update mean.py

--- AbigailAdamSteve/mean.py
+++ AbigailAdamSteve/mean.py
@@ -1,1 +1,20 @@
+import statistics
+import csv
+
+
+f = open("tempcovData.csv", newline='')
+reader = csv.reader(f)
+
+
+dataListedSeriesA = [int(column[1]) for column in reader]
+print(dataListedSeriesA)
+
+xValuesMean = statistics.mean(dataListedSeriesA)
+print("The mean is ", xValuesMean)
+
+
+
+
+
+
 
